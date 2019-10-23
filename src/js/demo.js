2c3e5084a391a064aa123a3fd0497e0c3037ea33
MANSI MEHTA <45642813+MANSI-MEHTA@users.noreply.github.com>
2019-10-22 22:21:21 -0400

    Create demo.js

---
+++ src/js/demo.js
@@ -0,0 +1,6 @@
+
+var mainDiv=$('#mainDiv')
+//var Div1="<div><ul id='myUL' > <li><span class='caret'>Nodes</span><ul id='ul_bev' class='nested'></ul></li> <li><span class='caret'>Enzymes</span><ul id='ul_tea' class='nested'></ul></li></ul></div>"
+// var Div1="<p> Mansi </Button>"
+// var jqDiv1=$(Div1);
+$("<p> Mansi </p>").appendTo("body");
